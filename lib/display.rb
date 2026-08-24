2e4f57396336e9ccb6759e7062ea08b1b16c5681
Cesar Quintana <c.evaristo.quintana@gmail.com>
2026-08-23 18:52:22 -0600

    nice display

--- lib/display.rb
+++ lib/display.rb
@@ -2,9 +2,16 @@ class Display
   def show_board(board = nil)
     puts "Nothing to show." unless board
     return unless board
-    puts "------"
+
+    board.width.times do |i|
+      print " #{i}"
+    end
+    print "\n"
+
     board.lines.each do |line|
-      puts line
+      print "|"
+      print line.join("|")
+      print "|\n"
     end
   end
 end
